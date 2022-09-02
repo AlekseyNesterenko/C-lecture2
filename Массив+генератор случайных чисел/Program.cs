2565e79f1c7fee2b60bffbcfd2148115faaa87fa
Aleksey Nesterenko <naa1205@bk.ru>
2022-09-02 13:37:35 +0700

    array+2 function

--- Массив+генератор случайных чисел/Program.cs
+++ Массив+генератор случайных чисел/Program.cs
@@ -19,9 +19,32 @@ void PrintArray(int[] col)  // метод 2
     position++;
     }
 }
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while(index<count)
+    {
+        if(collection[index]==find)
+        {
+        position = index;
+        break;
+        }
+        index++;
+    }
+    return position;
+}
+
 int [] array = new int[10]; //"создай массив из 10 элементов"
 
 FillArray(array);
+array[4] = 4;
+array[6] = 4;
 PrintArray(array);
+Console.WriteLine();
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
 
 // Заполенение и вывод массива
