d2f66db1df1d6dd7d2b53fbf0f6a2941a2a685b4
Aleksey Nesterenko <naa1205@bk.ru>
2022-09-02 13:25:56 +0700

    Array+random numbers

---
+++ Массив+генератор случайных чисел/Program.cs
@@ -0,0 +1,27 @@
+﻿void FillArray(int[] collection) // метод 1
+{
+int Length = collection.Length;
+int index = 0;
+while (index<Length)
+{
+collection[index] = new Random().Next(1, 10);
+index++;
+}
+}
+
+void PrintArray(int[] col)  // метод 2
+{
+    int count = col.Length;
+    int position = 0;
+    while (position<count)
+    {
+    Console.WriteLine(col[position]);
+    position++;
+    }
+}
+int [] array = new int[10]; //"создай массив из 10 элементов"
+
+FillArray(array);
+PrintArray(array);
+
+// Заполенение и вывод массива
