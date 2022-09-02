92c51f6459f0253a19941f6c538757464fb12f8e
Aleksey Nesterenko <naa1205@bk.ru>
2022-09-02 13:09:54 +0700

    Array+function

---
+++ Массив+функция(метод)/Program.cs
@@ -0,0 +1,16 @@
+﻿int [] array = {91,20,39,45,0,18,61,7,18};
+int n = array.Length;  // определяет количество элементов массива
+
+int find = 18;
+
+int index = 0;
+
+while (index<n)
+{
+    if(array[index]==find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
